df8020a3c1959cf0a6b6edff091f7a2afa25d38d
Dmitry Pleshchev <dmitrydevlax@yandex.ru>
2023-04-08 13:24:45 +0500

    Task 29 added

---
+++ Task 29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+
+int [] ints = new int[8];
+int length = ints.Length;
+
+Method(length);
+void Method(int length)
+{
+for (int i = 0; i <length; i++)
+{
+ints [i] = new Random().Next(-100,101);
+Console.Write($"{ints[i]}, ");
+}
+}
